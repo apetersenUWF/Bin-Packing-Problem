c1772aee692f78e54967528b6f40cbcf1b1f362a
aydenPC <ayden.petersen04@gmail.com>
2026-03-18 22:39:31 -0500

    work on node class

--- Bin-Packing-Problem/node.hpp
+++ Bin-Packing-Problem/node.hpp
@@ -10,15 +10,40 @@ template <typename P>
 class PriorityNode {
     P priority;
     bool maxPriority;
+    bool isMax() const;
     public:
     PriorityNode();
-    PriorityNode(T priority);
+    PriorityNode(P priority);
     PriorityNode(const PriorityNode* rhs);
     void operator=(PriorityNode* rhs);
-    void operator>(PriorityNode* rhs);
+    bool operator>(PriorityNode* rhs);
     void assignMaximumPriority();
     void setPriority(P priority);
-    bool isMax() const;
+    P getPriority() const;
 };
-
+    template <typename P>
+    PriorityNode<P>::PriorityNode() {maxPriority = false;}
+    template <typename P>
+    PriorityNode<P>::PriorityNode(P priority) {
+        this->priority = priority;
+        maxPriority = false;
+    }
+    template <typename P>
+    PriorityNode<P>::PriorityNode(const PriorityNode* rhs) {}
+    template <typename P>
+    void PriorityNode<P>::operator=(PriorityNode* rhs) {}
+    template <typename P>
+    bool PriorityNode<P>::operator>(PriorityNode* rhs) {
+        if (rhs->isMax()) return false;
+        if (maxPriority) return true;
+        return (priority > rhs-getPriority());
+    }
+    template <typename P>
+    void PriorityNode<P>::assignMaximumPriority() {maxPriority = true;}
+    template <typename P>
+    void PriorityNode<P>::setPriority(P priority) {this->priority = priority;}
+    template <typename P>
+    P PriorityNode<P>::getPriority() const {return priority;}
+    template <typename P>
+    bool PriorityNode<P>::isMax() const {return maxPriority;}
 #endif
